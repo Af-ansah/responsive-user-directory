f20a7378e98939b7792279b39088a737ccb3f0bc
Af-ansah <ansahgloria032@gmail.com>
2024-08-06 08:59:07 +0000

    feat: sort, html refactoring

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user.interface';
 
 @Component({
   selector: 'app-data-table',
@@ -8,10 +10,60 @@ import { Component } from '@angular/core';
   templateUrl: './data-table.component.html',
   styleUrl: './data-table.component.css'
 })
-export class DataTableComponent {
-data: any;
-sort(arg0: string) {
-throw new Error('Method not implemented.');
-}
+export class DataTableComponent implements OnInit {
+  data: User[] = [];
+  sortColumn: keyof User = 'name';
+  sortDirection: 'asc' | 'desc' = 'asc';
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.http.get<User[]>('https://jsonplaceholder.typicode.com/users')
+      .subscribe(response => {
+        this.data = response;
+        this.sortData();
+      });
+  }
+
+  sort(column: keyof User) {
+    if (this.sortColumn === column) {
+      this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    } else {
+      this.sortColumn = column;
+      this.sortDirection = 'asc';
+    }
+    this.sortData();
+  }
+
+ sortData() {
+    this.data.sort((a, b) => {
+      const valueA = this.getValue(a, this.sortColumn);
+      const valueB = this.getValue(b, this.sortColumn);
+      
+      if (valueA < valueB) return this.sortDirection === 'asc' ? -1 : 1;
+      if (valueA > valueB) return this.sortDirection === 'asc' ? 1 : -1;
+      return 0;
+    });
+  }
+
+  getValue(user: User, column: keyof User): any {
+    if (column === 'address') return user.address.city.toLowerCase();
+    if (column === 'company') return user.company.name.toLowerCase();
+    return typeof user[column] === 'string' ? (user[column] as string).toLowerCase() : user[column];
+  }
+
+  isColumnActive(column: keyof User): boolean {
+    return this.sortColumn === column;
+  }
+
+  getSortIcon(column: keyof User): string {
+    if (this.sortColumn === column) {
+      return this.sortDirection === 'asc' ? '↑' : '↓';
+    }
+    return '';
+  }
 }
